2a3edbb8a611cf79651812d70be9c8c331ee6ec5
adnanfauji <adnanfauji48@gmail.com>
2024-10-06 01:51:25 +0700

    Memodifikasi file reset password serta style.css

--- resetpassword.php
+++ resetpassword.php
@@ -5,33 +5,38 @@
     <meta charset="UTF-8" />
     <title>Lupa Kata Sandi</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/projectdesa/reset.css" />
-    <script>
+    <link rel="stylesheet" href="/projectdesa/style.css" />
+    <!-- <script>
         function handleSubmit(event) {
             event.preventDefault(); // Mencegah pengiriman formulir secara default
             // Ganti URL di bawah ini dengan URL yang sesuai
             window.location.href = '/projectdesa/confirm.php';
         }
-    </script>
+    </script> -->
 </head>
 
 <body>
-    <div class="container">
-        <div class="content">
-            <div class="image-box">
-                <img src="/projectdesa/img/contact.png" alt="Ilustrasi Kontak" />
+    <div class="wrapper">
+        <div class="left">
+                <img src="/projectdesa/img/contact.png" alt=""/>
+                <p>Setelah Anda mendapatkan tautan, klik pada tautan tersebut<br> dan  ikuti instruksi untuk membuat kata sandi baru.</p>
             </div>
-            <form action="" method="post" class="reset" onsubmit="handleSubmit(event)">
-                <div class="topic">Lupa sandi?</div>
-                <div class="topic2">Masukan email Anda dan kami akan mengirimkan tautan untuk mereset kata sandi Anda.</div>
-                <div class="input-box">
+            <form action="" method="post" class="login-wrapper" onsubmit="handleSubmit(event)">
+                <h1>Lupa sandi?</h1>
+                <p>Masukan email Anda dan kami akan mengirimkan tautan untuk mereset kata sandi Anda.</p>
+                <div class="input-field">
                     <input type="email" id="email" name="email" autocomplete="on" required />
                     <label for="email">Masukan email Anda</label>
                 </div>
-                <div class="input-box">
+                <!-- <div class="input-box">
                     <input type="submit" value="Kirim" />
+                </div> -->
+                <div>
+                    <a href="/projectdesa/confirm.php" id="login">
+                        <button type="submit" name="login" id="login" class="login">Kirim</button>
+                    </a>
                 </div>
-                <div class="cancel-link">
+                <div class="register">
                     <p><a href="/projectdesa/login.php">Cancel</a></p>
                 </div>
             </form>
